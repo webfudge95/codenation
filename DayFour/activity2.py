8e30e96a8458ccc4448fdfb16b7852298a924114
webfudge <webfudge95@gmail.com>
2021-03-18 10:58:13 +0000

    Activity Two

---
+++ DayFour/activity2.py
@@ -0,0 +1,11 @@
+'''
+Example of a for loop
+'''
+
+films = ["Lord of the rings", "Ghostbusters", "Back to the future", "The Hobbit"]
+
+for film in films:
+    if film.lower() == "ghostbusters":
+        print("Yay, Ghostbusters!")
+    else:
+        print("Boo! We want Ghostbusters")
