5ccf132a548b01430a0135c88d40d09dd15c285f
webfudge <webfudge95@gmail.com>
2021-03-19 13:36:18 +0000

    Moving to new folder structure

---
+++ noughts_crosses/drawgrid.py
@@ -0,0 +1,35 @@
+from os import system
+from random import randint
+
+def is_section(num):
+    if(num % 4) == 0:
+        return True
+    else:
+        return False
+
+vertical = "   {}   |   {}   |   {}   "
+horizontal = "-------|-------|-------"    
+
+def clear():
+    system("clear")
+
+def drawGrid(grid, squares):
+    clear()
+    print("Simple Noughts and Crosses Game, \n")
+    nums = [1,2,3]
+    for item in grid:
+        if is_section(item) == True:
+            print(horizontal)
+        elif item == 3 or item == 7 or item == 11:
+            a, b, c = nums
+            print(vertical.format(a, b, c))
+            nums = [x + 3 for x in nums]  
+        else:
+            if item == 2:
+                print(vertical.format(squares[0],squares[1],squares[2]))
+            elif item == 6:
+                print(vertical.format(squares[3],squares[4],squares[5]))
+            elif item == 10:
+                print(vertical.format(squares[6],squares[7],squares[8]))
+            else:
+                print(vertical.format(" "," "," "))
--- noughts_crosses/noughts_and_crosses.py
+++ noughts_crosses/noughts_and_crosses.py
@@ -1,41 +1,9 @@
 from os import system
 from random import randint
-
-def is_section(num):
-    if(num % 4) == 0:
-        return True
-    else:
-        return False
+import drawgrid as dg
 
 grid = range(1, 12)
 squares = [" "," "," "," "," "," "," "," "," "]
-
-def clear():
-    system("clear")
-
-vertical = "   {}   |   {}   |   {}   "
-horizontal = "-------|-------|-------"    
-
-def drawGrid():
-    clear()
-    print("Simple Noughts and Crosses Game, \n")
-    nums = [1,2,3]
-    for item in grid:
-        if is_section(item) == True:
-            print(horizontal)
-        elif item == 3 or item == 7 or item == 11:
-            a, b, c = nums
-            print(vertical.format(a,b,c))
-            nums = [x + 3 for x in nums]  
-        else:
-            if item == 2:
-                print(vertical.format(squares[0],squares[1],squares[2]))
-            elif item == 6:
-                print(vertical.format(squares[3],squares[4],squares[5]))
-            elif item == 10:
-                print(vertical.format(squares[6],squares[7],squares[8]))
-            else:
-                print(vertical.format(" "," "," "))
 
 def checkRange(cell1, cell2, cell3):
     if squares[cell1] ==  squares[cell2] and squares[cell2] == squares[cell3]:
@@ -48,7 +16,7 @@ def checkRange(cell1, cell2, cell3):
         
 def checkHorzontal():
     number = [0, 1, 2]
-    for item in range (1, 3):
+    for item in range (3):
         if checkRange(number[0], number[1], number[2]) == True:
             return True
         else:
@@ -56,7 +24,7 @@ def checkHorzontal():
 
 def checkVertical():
     number = [0, 3, 6]
-    for item in range (1, 3):
+    for item in range (3):
         if checkRange(number[0], number[1], number[2]) == True:
             return True
         else:
@@ -97,30 +65,35 @@ def userPlace():
                 userplace = int(input("\nChoose a number between 1 and 9: ")) - 1
                 break
             except:
-                drawGrid()
+                dg.drawGrid(grid, squares)
                 print("\nPlease choose an appropriate number!")
 
         if userplace > 9 or "-" in str(userplace):
-            drawGrid()
+            dg.drawGrid(grid, squares)
             print("\nPlease choose an appropriate number!")
         else:
             if squares[userplace] == " ":
                 squares[userplace] = "x"
                 check = False
             else:
-                drawGrid()
+                dg.drawGrid(grid, squares)
                 print("\nSpot Occupied!")
 
 def game():
-    drawGrid()
+    dg.drawGrid(grid, squares)
+    player = 1
+    computer = 2
+    order = randint(1,2)
+    print("{} will go first".format(order))
+
     while True:
         userPlace()
-        drawGrid()
+        dg.drawGrid(grid, squares)
         if checkWin() == True:
             print("\nWinner is Player")
             exit()
         computerPlace()
-        drawGrid()
+        dg.drawGrid(grid, squares)
         if checkWin() == True:
             print("\nWinner is Computer")
             exit()
